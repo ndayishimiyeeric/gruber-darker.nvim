7662c9e16783543a41a12020dadbdd650cff1141
Eric Ndayishimiye <ndayishimiyeeric86@gmail.com>
2025-12-20 18:08:42 +0100

    define config

---
+++ lua/gruberdarker/config.lua
@@ -0,0 +1,46 @@
+local M = {}
+
+---@class gruberdarker.Extensions
+---@field treesitter? boolean
+---@field gitsigns? boolean
+---@field telescope? boolean
+---@field whichkey? boolean
+---@field indentblankline? boolean
+---@field hop? boolean
+---@field leap? boolean
+---@field dashboard? boolean
+
+---@class gruberdarker.Config
+---@field transparent? boolean
+---@field italic_comments? boolean
+---@field terminal_colors? boolean
+---@field extensions? gruberdarker.Extensions
+local default_options = {
+    transparent = false,
+    italic_comments = false,
+    terminal_colors = true,
+    extensions = {
+        treesitter = true,
+        gitsigns = true,
+        telescope = true,
+        whichkey = true,
+        indentblankline = true,
+        hop = true,
+        leap = true,
+        dashboard = true,
+    },
+}
+
+---@type gruberdarker.Config
+M.options = {}
+
+---@param options gruberdarker.Config|nil
+function M.setup(options)
+    options = options or {}
+    M.options = vim.tbl_extend("force", {}, default_options, options)
+    vim.g.gruberdarker_options = M.options
+end
+
+M.setup()
+
+return M
